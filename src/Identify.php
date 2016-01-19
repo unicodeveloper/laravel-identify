76bd67dcaf3231846201cda1af0720f110c47b4f
busayo <prosperotemuyiwa@gmail.com>
2016-01-19 05:09:29 +0100

    feat(package): Add language, os and browser methods

--- src/Identify.php
+++ src/Identify.php
@@ -3,16 +3,66 @@
 namespace Unicodeveloper\Identify;
 
 use Sinergi\BrowserDetector\Browser;
+use Sinergi\BrowserDetector\Os;
+use Sinergi\BrowserDetector\Language;
 
 class Identify {
 
+    /**
+     * Store the browser object
+     * @var object
+     */
+    protected $browser;
+
+    /**
+     * Store the os object
+     * @var object
+     */
+    protected $os;
+
+    /**
+     * Store the language object
+     * @var object
+     */
+    protected $language;
+
+    /**
+     *  Create an Instance of Browser and Os
+     */
     public function __construct()
     {
+        $this->os = new Os();
         $this->browser = new Browser();
+        $this->language = new Language();
     }
 
+    /**
+     * Get all the methods applicable to browser detection
+     * e.g getName(), getVersion()
+     * @return \Sinergi\BrowserDetector\Browser
+     */
     public function browser()
     {
-        return $this->browser->getName();
+        return $this->browser;
     }
+
+    /**
+     * Get all the methods applicable to Os detection
+     * e.g getName(), getVersion()
+     * @return \Sinergi\BrowserDetector\Os
+     */
+    public function os()
+    {
+        return $this->os;
+    }
+
+    /**
+     * Get all the methods applicable to Language detection
+     * @return \Sinergi\BrowserDetector\Language
+     */
+    public function lang()
+    {
+        return $this->language;
+    }
+
 }
