926dca46084ccf049d5a607796aada622f5afe08
busayo <prosperotemuyiwa@gmail.com>
2016-01-19 05:43:08 +0100

    feat(device): Add device detection functionality

--- src/Identify.php
+++ src/Identify.php
@@ -2,23 +2,30 @@
 
 namespace Unicodeveloper\Identify;
 
-use Sinergi\BrowserDetector\Browser;
 use Sinergi\BrowserDetector\Os;
+use Sinergi\BrowserDetector\Device;
+use Sinergi\BrowserDetector\Browser;
 use Sinergi\BrowserDetector\Language;
 
 class Identify {
-
-    /**
-     * Store the browser object
-     * @var object
-     */
-    protected $browser;
 
     /**
      * Store the os object
      * @var object
      */
     protected $os;
+
+    /**
+     * Store the device object
+     * @var object
+     */
+    protected $device;
+
+    /**
+     * Store the browser object
+     * @var object
+     */
+    protected $browser;
 
     /**
      * Store the language object
@@ -32,18 +39,10 @@ class Identify {
     public function __construct()
     {
         $this->os = new Os();
+        $this->device = new Device();
         $this->browser = new Browser();
         $this->language = new Language();
-    }
 
-    /**
-     * Get all the methods applicable to browser detection
-     * e.g getName(), getVersion()
-     * @return \Sinergi\BrowserDetector\Browser
-     */
-    public function browser()
-    {
-        return $this->browser;
     }
 
     /**
@@ -54,6 +53,26 @@ class Identify {
     public function os()
     {
         return $this->os;
+    }
+
+    /**
+     * Get all the methods applicable to Device detection
+     * e.g getName()
+     * @return \Sinergi\BrowserDetector\Device
+     */
+    public function device()
+    {
+        return $this->device;
+    }
+
+    /**
+     * Get all the methods applicable to Browser detection
+     * e.g getName(), getVersion()
+     * @return \Sinergi\BrowserDetector\Browser
+     */
+    public function browser()
+    {
+        return $this->browser;
     }
 
     /**
