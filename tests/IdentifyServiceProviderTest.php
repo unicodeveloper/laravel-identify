2ec7aed163e142eff86b436f847134fc3bb672d5
perfectmak <damiperfect@gmail.com>
2016-04-21 17:24:46 +0100

    Updated IdentifyServiceProviderTest to properly test for service register

--- tests/IdentifyServiceProviderTest.php
+++ tests/IdentifyServiceProviderTest.php
@@ -2,8 +2,9 @@
 
 namespace Unicodeveloper\Identify\Test;
 
-use Mockery\MockInterface;
+use \Mockery as m;
 use PHPUnit_Framework_TestCase;
+use Unicodeveloper\Identify\Identify;
 use Unicodeveloper\Identify\IdentifyServiceProvider;
 
 class IdentifyServiceProviderTest extends PHPUnit_Framework_TestCase
@@ -22,16 +23,21 @@ class IdentifyServiceProviderTest extends PHPUnit_Framework_TestCase
     protected function setUp()
     {
         parent::setUp();
-        $this->mockApp = \Mockery::mock();
+        $this->mockApp = m::mock('\Illuminate\Contracts\Foundation\Application');
         $this->provider = new IdentifyServiceProvider($this->mockApp);
     }
 
     /**
-     * Test that it provides the correct service name
+     * Test that it registers the correct service name with Identity
      */
     public function testRegister()
     {
-        $this->mockApp->shouldReceive('bind')->once()->withAnyArgs()->once();
+        $this->mockApp->shouldReceive('bind')
+            ->once()
+            ->andReturnUsing(function($name, $closure) {
+                $this->assertEquals('laravel-identify', $name);
+                $this->assertInstanceOf(Identify::class, $closure());
+            });
 
         $this->provider->register();
     }
